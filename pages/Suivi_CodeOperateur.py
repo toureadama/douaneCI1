033c042f4c90316fe3a5671de0a76e1766f1e5ab
toureadama <toureadama@gmail.com>
2023-05-23 12:46:02 +0000

    Affichage de l'indice par défaut

--- pages/Suivi_CodeOperateur.py
+++ pages/Suivi_CodeOperateur.py
@@ -32,7 +32,7 @@ df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
 Operateur = st.sidebar.selectbox(
     'Code Opérateur',
-    df['Code Opérateur'].unique())
+    df['Code Opérateur'].unique(), index=10)
 
 if Operateur:
     PosTarif = st.sidebar.selectbox(
