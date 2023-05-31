c39651d76ad4ca43d5e5f07a236db262d46a8359
toureadama <toureadama@gmail.com>
2023-05-31 13:25:21 +0000

    maj des fichiers globaux

--- pages/Controle.py
+++ pages/Controle.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+
 update = False
 
-@st.cache_resource
+# Chargement des fichiers contenant déjà les variables retraitées
+@st.cache_resource 
 def load_all_file(update):
     df_CIAB1     = pd.read_csv('df_CIAB1.csv')
     df_Scan      = pd.read_csv('df_Scan.csv')
@@ -53,31 +55,18 @@ Val_moy = df[
     & (df["Sous_Produit"] == Libelle) 
     & (df["Origine"] == Origin)]["PU_moy"].unique()[0]
 
-#st.write(f"La valeur FOB moyenne doit être **:blue[{PdsNet * Val_moy:.0f}]** FCFA :sunglasses:")
 st.write(f"La valeur FOB moyenne doit être:")
 st.header(f"**:blue[{PdsNet * Val_moy:,.0f}]** FCFA")
-#st.header(f"**:blue[{PdsNet * Val_moy:,d}]** FCFA")
-# st.write(f":sunglasses:") 
 
 Comp = df[
     (df["Produit"] == PosTarif) 
     & (df["Sous_Produit"] == Libelle) 
     & (df["Origine"] == Origin)]
 
-# Modiffication pour l'affichage de certaines données
-#col_dec = ['D&T', 'Val FOB', 'Val CAF', 'Pds Brut', 'Pds Net', 'fret']
-#Comp.loc[:, 'D&T']      = Comp['D&T'].map('{:,d}'.format)
-#Comp.loc[:, 'Val FOB']  = Comp['Val FOB'].map('{:,d}'.format)
-#Comp.loc[:, 'Val CAF']  = Comp['Val CAF'].map('{:,d}'.format)
-#Comp.loc[:, 'Pds Brut'] = Comp['Pds Brut'].map('{:,d}'.format)
-#Comp.loc[:, 'Pds Net']  = Comp['Pds Net'].map('{:,d}'.format)
-#Comp.loc[:, 'fret']     = Comp['fret'].map('{:,d}'.format)
-
-#col_mille = ['D&T_tx', 'PU', 'PU_moy', 'Ecart']
-#Comp.loc[:, 'D&T_tx'] = Comp['D&T_tx'].map('{:.2f}'.format)
-#Comp.loc[:, 'PU']     = Comp['PU'].map('{:.2f}'.format)
-#Comp.loc[:, 'PU_moy'] = Comp['PU_moy'].map('{:.2f}'.format)
-#Comp.loc[:, 'Ecart']  = Comp['Ecart'].map('{:.2f}'.format)
+Comp.loc[:, "Pds Net Rel"] = abs(Comp.loc[:, "Pds Net"] - PdsNet)
+Comp.sort_values(by=["Pds Net Rel"], inplace=True)
+Comp.drop(columns=["Pds Net Rel"], inplace=True)
+Comp.loc[:, 'Pds Net']  = Comp['Pds Net'].map('{:,d}'.format)
 
 st.write(f"Quelques exemples de déclarations de la même catégorie.")
 st.write(Comp.T)
--- pages/Suivi_CodeOperateur.py
+++ pages/Suivi_CodeOperateur.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pandas as pd
 
+
 update = False
 
 @st.cache_resource
