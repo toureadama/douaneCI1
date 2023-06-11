b7fce23e69b9a67cd0b74b3b757992a5c6f17e71
toureadama <toureadama@gmail.com>
2023-06-11 19:16:54 +0000

    Origi

--- Variation_forte.py
+++ Variation_forte.py
@@ -4,10 +4,10 @@ from st_pages import Page, show_pages, hide_pages
 
 show_pages([
     Page("Variation_forte.py","Accueil"),
-    #Page("pages/Variation.py","Variation"),
-    #Page("pages/Variation_CIAB1.py","Variation1"),
-    #Page("pages/Variation_CIAB3.py","Variation3"),
-    #Page("pages/Variation_CIAB6.py","Variation6")
+    Page("pages/Variation.py","Variation"),
+    Page("pages/Variation_CIAB1.py","Variation1"),
+    Page("pages/Variation_CIAB3.py","Variation3"),
+    Page("pages/Variation_CIAB6.py","Variation6")
 ])
 
 # Listes des mots de passe par département
@@ -17,23 +17,19 @@ list_MDP_CIAB3 = ['BONJOUR3', 'BONSOIR3', 'CIAB3']
 list_MDP_CIAB6 = ['BONJOUR6', 'BONSOIR6', 'CIAB6']
 
 
-#hide_pages(['Accueil', 'Variation', 'Variation1', 'Variation3', 'Variation6'])
+hide_pages(['Accueil', 'Variation', 'Variation1', 'Variation3', 'Variation6'])
 
 st.text_input("Votre mot de passe", key="name", type='password')
 
 # You can access the value at any point with:
 if st.session_state.name in list_MDP:
-    st.write("ok 0")
-    #switch_page('Variation')
+    switch_page('Variation')
 elif st.session_state.name in list_MDP_CIAB1:
-    st.write("ok 1")
-    #switch_page('Variation1')
+    switch_page('Variation1')
 elif st.session_state.name in list_MDP_CIAB3:
-    st.write("ok 3")
-    #switch_page('Variation3')
+    switch_page('Variation3')
 elif st.session_state.name in list_MDP_CIAB6:
-    st.write("ok 6")
-    #switch_page('Variation6')
+    switch_page('Variation6')
 else:
     st.write("Mot de passe incorrect")
     
