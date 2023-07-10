e0b46027945f60602a6468eb5fcdbb34f7a19e9c
toureadama <toureadama@gmail.com>
2023-07-10 15:32:46 +0000

    modif des f

--- Variation_forte.py
+++ Variation_forte.py
@@ -20,7 +20,7 @@ list_MDP_CIAB5 = ['BONJOUR5', 'BONSOIR5', 'CIAB5']
 list_MDP_CIAB6 = ['BONJOUR6', 'BONSOIR6', 'CIAB6']
 list_MDP_CIAB7 = ['BONJOUR7', 'BONSOIR7', 'CIAB7']
 
-hide_pages(['Accueil', 'Variation', 'Variation1', 'Variation3', 'Variation6'])
+hide_pages(['Accueil', 'Variation', 'Variation1', 'Variation3', 'Variation5', 'Variation6', 'Variation7'])
 
 st.text_input("Votre mot de passe", key="name", type='password')
 
