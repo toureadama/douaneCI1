22e52ed98673d6ac1d6fb1bcc995a225b45adccf
toureadama <toureadama@gmail.com>
2023-10-01 10:43:50 +0000

    modif cont glob

--- pages/ControleCIAB7.py
+++ pages/ControleCIAB7.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from io import BytesIO
 from st_pages import Page, show_pages
 
 show_pages([
@@ -93,11 +94,22 @@ if NumDecla :
     st.write(f"Quelques exemples de déclarations de la même catégorie.")
     st.write(Comp.T)
 
-    csv = Comp.to_csv(index=False).encode('utf-8')
+    output = BytesIO()
 
-    download1 = st.download_button(
-        label="Export sous CSV",
-        data=csv,
-        file_name='Sortie.csv',
-        mime='text/csv'
+    def to_excel2(df):
+        in_memory_fp = BytesIO()
+        df.to_excel(in_memory_fp)
+        # Write the file out to disk to demonstrate that it worked.
+        in_memory_fp.seek(0, 0)
+        return in_memory_fp.read()
+
+    excel_data = to_excel2(Comp)
+    file_name = "sortie.xlsx"
+    st.download_button(
+        f"Exporter sous Excel",
+        excel_data,
+        file_name,
+        f"text/{file_name}",
+        key=file_name
     )
+    
