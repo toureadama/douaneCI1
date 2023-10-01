bcc34f8a8cfb25ef985e289242614df81178bcd5
toureadama <toureadama@gmail.com>
2023-10-01 10:22:15 +0000

    modif Xl

--- pages/ControleCIAB1.py
+++ pages/ControleCIAB1.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from io import BytesIO
 from st_pages import Page, show_pages
 
 show_pages([
@@ -113,5 +114,24 @@ if NumDecla :
         data=csv,
         file_name='Sortie.csv',
         mime='text/csv'
+    )
+
+    output = BytesIO()
+
+    def to_excel2(df):
+        in_memory_fp = BytesIO()
+        df.to_excel(in_memory_fp)
+        # Write the file out to disk to demonstrate that it worked.
+        in_memory_fp.seek(0, 0)
+        return in_memory_fp.read()
+
+    excel_data = to_excel2(df)
+    file_name = "sortie.xlsx"
+    st.download_button(
+        f"Exporter {file_name}",
+        excel_data,
+        file_name,
+        f"text/{file_name}",
+        key=file_name
     )
     
