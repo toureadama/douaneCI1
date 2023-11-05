cf77fd3f767bf6e06f43be58bf0274e528c1e9bb
toureadama <toureadama@gmail.com>
2023-11-05 23:34:00 +0100

    test allou maj 18

--- read_xl2.py
+++ read_xl2.py
@@ -9,23 +9,6 @@ df = pd.read_csv("TEC_CEDEAO_SH_2017_LR_TAXES.csv")
 #st.dataframe(df)
 st.write(df.head())
 
-
-def to_excel(df):
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1)  
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
-df_xlsx = to_excel(df)
-st.download_button(label='📥 Download Current Result',
-                                data=df_xlsx ,
-                                file_name= 'df_test.xlsx')
-
 def to_excel2(df):
     in_memory_fp = BytesIO()
     df.to_excel(in_memory_fp)
