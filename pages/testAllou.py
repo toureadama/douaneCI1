79b3165d35c197481948ebdba2e850d4a8e487cd
toureadama <toureadama@gmail.com>
2023-11-05 20:24:33 +0100

    test allou maj 12

--- pages/testAllou.py
+++ pages/testAllou.py
@@ -31,21 +31,12 @@ st.write('Nombre de champs concernés:', resultDesMarch.shape[0])
 
 # Extraction sous Excel
 
-output = BytesIO()
+csv = resultDesMarch.to_csv(index=False).encode('utf-8')
 
-def to_excel2(df):
-    in_memory_fp = BytesIO()
-    df.to_excel(in_memory_fp)
-    # Write the file out to disk to demonstrate that it worked.
-    in_memory_fp.seek(0, 0)
-    return in_memory_fp.read()
-
-excel_data = to_excel2(resultDesMarch)
-file_name = "sortie.xlsx"
-st.download_button(
-    f"Exporter sous Excel",
-    excel_data,
-    file_name,
-    f"text/{file_name}",
-    key=file_name
+# download button 1 to download dataframe as csv
+download1 = st.download_button(
+    label="Export sous CSV",
+    data=csv,
+    file_name='Sortie.csv',
+    mime='text/csv'
 )
