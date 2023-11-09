9f103617063af46fe136b716f9bb167b44283496
toureadama <toureadama@gmail.com>
2023-11-09 23:21:07 +0100

    Ajout analyse espèces 10

--- pages/testKarimCodeOpeUnique.py
+++ pages/testKarimCodeOpeUnique.py
@@ -23,7 +23,7 @@ dpp = load_file(update)
 
 dpp = dpp.loc[:, ~dpp.columns.str.contains('^Unnamed')]
 
-st.write(dpp.columns)
+st.write(len(dpp['Code opérateur'].unique()))
 
 #codop = st.sidebar.selectbox(
     #'Choisir le code opérateur',
