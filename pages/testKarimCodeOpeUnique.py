28821a3e903dec2976c4f561171a4d882a0e9f1f
toureadama <toureadama@gmail.com>
2023-11-09 23:17:58 +0100

    Ajout analyse espèces 8

--- pages/testKarimCodeOpeUnique.py
+++ pages/testKarimCodeOpeUnique.py
@@ -23,24 +23,24 @@ dpp = load_file(update)
 
 dpp = dpp.loc[:, ~dpp.columns.str.contains('^Unnamed')]
 
-st.write(dpp.columns)
+st.write(dpp.shape)
 
-codop = st.sidebar.selectbox(
-    'Choisir le code opérateur',
-    dpp['Code opérateur'].unique())
+#codop = st.sidebar.selectbox(
+    #'Choisir le code opérateur',
+    #dpp['Code opérateur'].unique())
 
-resultcodop = dpp[dpp['Code opérateur']==codop]
-resultcodop = resultcodop[['DESCRIPTION MARCHANDISE', 'Origine', 'PU REC', 'N°Déclaration REC']].drop_duplicates()
-st.dataframe(resultcodop, use_container_width=True)
+#resultcodop = dpp[dpp['Code opérateur']==codop]
+#resultcodop = resultcodop[['DESCRIPTION MARCHANDISE', 'Origine', 'PU REC', 'N°Déclaration REC']].drop_duplicates()
+#st.dataframe(resultcodop, use_container_width=True)
 
-st.write("Nombre de déclarations trouvées :", f"{resultcodop.shape[0]}")
+#st.write("Nombre de déclarations trouvées :", f"{resultcodop.shape[0]}")
 
-csv = resultcodop.to_csv(index=False).encode('utf-8')
+#csv = resultcodop.to_csv(index=False).encode('utf-8')
 
-# download button 1 to download dataframe as csv
-download1 = st.download_button(
-    label="Export sous CSV",
-    data=csv,
-    file_name='SortieFrm.csv',
-    mime='text/csv'
-)
+## download button 1 to download dataframe as csv
+#download1 = st.download_button(
+    #label="Export sous CSV",
+    #data=csv,
+    #file_name='SortieFrm.csv',
+    #mime='text/csv'
+#)
