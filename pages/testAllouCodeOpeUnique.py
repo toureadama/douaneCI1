4c6446935a09884ba24e03a57c3e227b10782214
toureadama <toureadama@gmail.com>
2023-11-10 09:18:05 +0100

    test affichage 21

--- pages/testAllouCodeOpeUnique.py
+++ pages/testAllouCodeOpeUnique.py
@@ -36,4 +36,25 @@ fin   = st.sidebar.date_input("Date de fin:", value=date_max)
 dpp = dpp[(dpp['Date de la déclaration'] >= pd.to_datetime(debut)) & 
           (dpp['Date de la déclaration'] <= pd.to_datetime(fin))]
 
-st.write(dpp.columns)
+#st.write(dpp.columns)
+
+codop = st.sidebar.selectbox(
+    'Choisir le code opérateur',
+    dpp['Code opérateur'].unique())
+
+resultcodop = dpp[dpp['Code opérateur']==codop]
+resultcodop = resultcodop.drop_duplicates()
+
+st.dataframe(resultcodop, use_container_width=True)
+
+st.write("Nombre de déclarations trouvées :", f"{resultcodop.shape[0]}")
+
+csv = resultcodop.to_csv(index=False).encode('utf-8')
+
+# download button 1 to download dataframe as csv
+download1 = st.download_button(
+    label="Export sous CSV",
+    data=csv,
+    file_name='SortieFrm.csv',
+    mime='text/csv'
+)
