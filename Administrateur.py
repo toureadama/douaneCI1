2c17b65348e7e3b899e3606ac490d1e703854067
toureadama <toureadama@gmail.com>
2023-11-27 15:01:58 +0100

    maj pip

--- Administrateur.py
+++ Administrateur.py
@@ -1,7 +1,9 @@
 import pandas as pd
-import mysql.connector  # version  mysql.connector-2.2.9
+import mysql.connector 
 import streamlit as st
 from st_pages import Page, show_pages
+from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 show_pages([
     Page("Variation_forte.py","Accueil"),
@@ -24,7 +26,7 @@ def main():
     st.title("Opérations de l'administrateur d'utilisateurs")
 
     # Display Options for CRUD Operations
-    option=st.sidebar.selectbox("Sélectionner une opération",("Créer","Lire","Modifier","Supprimer"))
+    option=st.sidebar.selectbox("Sélectionner une opération",("Créer","Lire","Modifier"))
     # Perform Selected CRUD Operations
     if option=="Créer":
         st.subheader("Créer un nouvel utilisateur")
@@ -41,16 +43,16 @@ def main():
         nom=st.text_input("Nom",'')
         prenom=st.text_input("Prénom")
         bur=st.selectbox("Bureau", list(resultBur.loc[:,1]))
-        identifiant=st.text_input("Identifiant de connexion")
-        password=st.text_input("Mot de passe")
         bdd=st.selectbox("Base de données", list(resultBDD.loc[:,1]))
-        if bdd == 'Decisionnel':
+        if bdd == 'Décisionnel':
             acc=st.selectbox("Privilège d'accès", ['Manager', 'Vérificateur'])
         if bdd == 'RFCV':
             acc=st.selectbox("Privilège d'accès", ['CB', 'CV'])
+        identifiant=st.text_input("Identifiant de connexion")
+        password=st.text_input("Mot de passe")
         if st.button("Créer"):
-            sql= "insert into utilisateur(nom,prenom,bureau,identifiant,password,bdd,acces) values(%s,%s,%s,%s,%s,%s,%s)"
-            val= (nom,prenom,bur,identifiant,password,bdd,acc)
+            sql= "insert into utilisateur(nom,prenom,bureau,bdd,acces,identifiant,password) values(%s,%s,%s,%s,%s,%s,%s)"
+            val= (nom,prenom,bur,bdd,acc,identifiant,password)
             mycursor.execute(sql,val)
             mydb.commit()
             st.success("Enregistrement réussi!!!")
@@ -61,9 +63,29 @@ def main():
         st.subheader("Les utilisateurs dans la base")
         mycursor.execute("select * from utilisateur")
         result = mycursor.fetchall()
-        result = pd.DataFrame(result, columns=['ID','Nom','Prénom','Bureau','Identifiant','Mot_de_passe','Base_de_donnees', 'Habilitation'])
-        st.dataframe(result)
+        result = pd.DataFrame(result, columns=['ID','Nom','Prénom','Bureau','Base_de_donnees', 'Habilitation','Identifiant','Mot_de_passe'])
+        
+        gd = GridOptionsBuilder.from_dataframe(result)
+        gd.configure_pagination(enabled=True)
+        gd.configure_default_column(editable=True, groupable=True)
+        gd.configure_selection(selection_mode="multiple", use_checkbox=True)
+        gridoptions = gd.build()
+        grid_table = AgGrid(
+            result,
+            gridOptions=gridoptions,
+            update_mode=GridUpdateMode.SELECTION_CHANGED,
+            theme="material",
+        )
 
+        df_sel_row = pd.DataFrame(grid_table["selected_rows"])
+
+        if df_sel_row.shape[0] > 0:
+            if st.button("Supprimer"):
+                for id in df_sel_row['ID']:
+                    sql="delete from utilisateur where id =%s"
+                    val=(id,)
+                    mycursor.execute(sql,val)
+                    mydb.commit()
 
 
     elif option=="Modifier":
@@ -71,44 +93,28 @@ def main():
         id=st.number_input("Enter ID",min_value=1)
 
         mycursor.execute("select * from utilisateur")
-        result = pd.DataFrame(mycursor.fetchall(), columns=['ID','Nom','Prénom','Bureau','Identifiant','Mot_de_passe','Base_de_donnees', 'Habilitation'])
+        result = pd.DataFrame(mycursor.fetchall(), columns=['ID','Nom','Prénom','Bureau','Base_de_donnees', 'Habilitation','Identifiant','Mot_de_passe'])
         result = result[result.ID==id]
 
         if result.shape[0]==1:
             nom=st.text_input("nouveau Nom", result['Nom'].iloc[0])
             prenom=st.text_input("nouveau Prenom", result['Prénom'].iloc[0])
             bur=st.text_input("nouveau Bureau", result['Bureau'].iloc[0])
-            identifiant=st.text_input("nouvel Identifiant de connexion", result['Identifiant'].iloc[0])
-            password=st.text_input("nouveau Mot de passe", result['Mot_de_passe'].iloc[0])
             bdd=st.text_input("Base de données", result['Base_de_donnees'].iloc[0])
             acc=st.text_input("Privilège d'accès", result['Habilitation'].iloc[0])
+            identifiant=st.text_input("nouvel Identifiant de connexion", result['Identifiant'].iloc[0])
+            password=st.text_input("nouveau Mot de passe", result['Mot_de_passe'].iloc[0])
             if st.button("Modifier"):
-                sql="update utilisateur set nom=%s, prenom=%s, bureau=%s, identifiant=%s, password=%s, bdd=%s, acces=%s where id =%s"
-                val= (nom,prenom,bur,identifiant,password,bdd,acc,id)
+                sql="update utilisateur set nom=%s, prenom=%s, bureau=%s, bdd=%s, acces=%s, identifiant=%s, password=%s where id =%s"
+                val= (nom,prenom,bur,bdd,acc,identifiant,password,id)
                 mycursor.execute(sql,val)
                 mydb.commit()
                 st.success("Mise à jour réussie!!!")
         else:
             st.write("Cet identifiant n'est pas créé.")
 
-
-
-    elif option=="Supprimer":
-        st.subheader("Supprimer un enregistrement")
-        id=st.number_input("Entrer l'ID",min_value=1)
-        if st.button("Supprimer"):
-            sql="delete from utilisateur where id =%s"
-            val=(id,)
-            mycursor.execute(sql,val)
-            mydb.commit()
-            st.success("Suppression réussie!!!")
-
-
 if mycursor:
     if __name__ == "__main__":
         main()
 else:
     st.write("Connexion impossible à établir")
-
-
-
--- Variation_forte.py
+++ Variation_forte.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import mysql.connector
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 from st_pages import Page, show_pages, hide_pages
@@ -23,18 +25,6 @@ show_pages([
     Page("pages/Variation_CIABP.py","VariationP")
 ])
 
-# Listes des mots de passe par département avec des tests.
-list_MDP_NIV1  = ['OUI' , 'NIV1' ]
-list_MDP       = ['BONJOUR' , 'BONSOIR', 'X', 'Y', 'Z' ]
-list_MDP_CIAB1 = ['BONJOUR1', 'BONSOIR1', 'A', 'B', 'C', 'D', 'E']
-list_MDP_CIAB3 = ['BONJOUR3', 'BONSOIR3', 'CIAB3']
-list_MDP_CIAB5 = ['BONJOUR5', 'BONSOIR5', 'CIAB5']
-list_MDP_CIAB6 = ['BONJOUR6', 'BONSOIR6', 'CIAB6']
-list_MDP_CIAB7 = ['BONJOUR7', 'BONSOIR7', 'CIAB7']
-list_MDP_CIABP = ['BONJOURP', 'BONSOIRP', 'CIABP']
-list_MDP_ALLOU  = ['BAP']
-list_MDP_KARIM  = ['CV']
-list_MDP_ADMIN  = ['SERGE']
 
 hide_pages(['Accueil', 'Admin', 'Contrôle 1er niveau', 'Variation', 'Variation1', 
             'Variation3', 'Variation5', 'Variation6', 'Variation7', 'VariationP','XL2',
@@ -42,31 +32,54 @@ hide_pages(['Accueil', 'Admin', 'Contrôle 1er niveau', 'Variation', 'Variation1
              "Analyse Espèces", "Analyse CC", "Contrôle Code Opérateur 2"
              ]) # 
 
-st.text_input("Votre mot de passe", key="name", type='password')
 
-# You can access the value at any point with:
-if st.session_state.name in list_MDP:
-    switch_page('Variation')
-elif st.session_state.name in list_MDP_ADMIN:
-    switch_page('Admin') 
-elif st.session_state.name in list_MDP_NIV1:
-    switch_page('Contrôle 1er niveau') 
-elif st.session_state.name in list_MDP_ALLOU:
-    switch_page('Recherche')   
-elif st.session_state.name in list_MDP_KARIM:
-    switch_page('PU REC')   
-elif st.session_state.name in list_MDP_CIAB1:
-    switch_page('Variation1')
-elif st.session_state.name in list_MDP_CIAB3:
-    switch_page('Variation3')
-elif st.session_state.name in list_MDP_CIAB5:
-    switch_page('Variation5')
-elif st.session_state.name in list_MDP_CIAB6:
-    switch_page('Variation6')
-elif st.session_state.name in list_MDP_CIAB7:
-    switch_page('Variation7')
-elif st.session_state.name in list_MDP_CIABP:
-    switch_page('VariationP')
+#Etablir la connexion
+mydb = mysql.connector.connect(
+    host = 'sql11.freemysqlhosting.net',
+    user = 'sql11664568',
+    password = 'fMJHRX62M7',
+    database = 'sql11664568'
+)
+
+mycursor = mydb.cursor()
+
+st.title('Bienvenue sur le page de connexion!')
+
+st.subheader("Les utilisateurs dans la base")
+mycursor.execute("select * from utilisateur")
+result = mycursor.fetchall()
+result = pd.DataFrame(result, columns=['ID','Nom','Prénom','Bureau','Base_de_donnees', 'Habilitation','Identifiant','Mot_de_passe'])
+st.dataframe(result)
+new_list = list(zip(list(result["Identifiant"]), list(result["Mot_de_passe"])))
+new_list_acces = list(zip(list(result["Bureau"]), list(result["Base_de_donnees"]), list(result["Habilitation"])))
+
+identifiant = st.text_input("Votre identifiant", key="name")
+mdp = st.text_input("Votre mot de passe", key="password", type="password")
+
+if (identifiant, mdp) in new_list:
+    ind = new_list.index((identifiant, mdp))
+    st.write(new_list_acces[ind][0:3])
+    if (new_list_acces[ind][0:3]==('CIAB1', 'Décisionnel', 'Vérificateur')) or (new_list_acces[ind][0:3]==('Scanner', 'Décisionnel', 'Vérificateur')):
+        switch_page('Variation1')
+    elif new_list_acces[ind][0:3]==('CIAB3', 'Décisionnel', 'Vérificateur'):
+        switch_page('Variation3')
+    elif new_list_acces[ind][0:3]==('CIAB6Neuf', 'Décisionnel', 'Vérificateur'):
+        switch_page('Variation6')
+    elif new_list_acces[ind][0:3]==('BRP', 'Décisionnel', 'Vérificateur'):
+        switch_page('Variation5')
+    elif new_list_acces[ind][0:3]==('CIABP', 'Décisionnel', 'Vérificateur'):
+        switch_page('VariationP')
+    elif new_list_acces[ind][1:3]==('Décisionnel', 'Manager'):
+        switch_page('Contrôle 1er niveau') 
+    elif new_list_acces[ind][0:3]==('TOUT', 'Décisionnel', 'Vérificateur'):
+        switch_page('Variation')
+    elif (new_list_acces[ind][0:3]==('CIAB1', 'RFCV', 'CV')) or (new_list_acces[ind][0:3]==('Scanner', 'RFCV', 'CV')):
+        switch_page('PU REC')
+    elif (new_list_acces[ind][0:3]==('CIAB1', 'RFCV', 'CB')) or (new_list_acces[ind][0:3]==('Scanner', 'RFCV', 'CB')):
+        switch_page('Recherche')   
+    elif new_list_acces[ind][0]=='ADMIN':
+        switch_page('Admin')
+    else:
+        st.write("Cet utilisateur n'a pas d'accès défini")
 else:
     st.write("Mot de passe incorrect")
-    
