e586546557d1ca661367bf42e006403b526d6e04
toureadama <toureadama@gmail.com>
2023-11-28 12:07:52 +0100

    maj Admin34

--- Administrateur.py
+++ Administrateur.py
@@ -97,7 +97,8 @@ def main():
         id=st.number_input("Enter ID",min_value=1)
 
         mycursor.execute("select * from utilisateur")
-        result = pd.DataFrame(mycursor.fetchall())#, columns=['ID','Nom','Prénom','Bureau','Base_de_donnees', 'Habilitation','Identifiant','Mot_de_passe'])
+        result = mycursor.fetchall()
+        result = pd.DataFrame(result)
         result = result[result.ID==id]
 
 
